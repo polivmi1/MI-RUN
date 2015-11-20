ea28bb753059b822b48c1b3df6e6b7d639d0742d
polivmi1 <polivmi1@fit.cvut.cz>
2015-11-20 15:35:03 +0100

    Added native readType

--- VM/NativeFunctions.C
+++ VM/NativeFunctions.C
@@ -19,11 +19,21 @@ bool NativeFunctions::callNative(std::string name, DataStack *dataStack, Heap *h
 		Instance * a = heap->getInstance(dataStack->pop());
 		std::cout << "PRINT: " << a->toString() << std::endl;
 	}else if(name == "readInt"){
-	
+		int x;
+		std::cin >> x;
+		Instance * a = new InstanceINT(x);
+		dataStack->push(heap->addInstance(a));		
 	}else if(name == "readDouble"){
+		double x;
+		std::cin >> x;
+		Instance * a = new InstanceDOUBLE(x);
+		dataStack->push(heap->addInstance(a));		
 	
 	}else if(name == "readString"){
-	
+		std::string x;
+		std::cin >> x;
+		Instance * a = new InstanceSTRING(x);
+		dataStack->push(heap->addInstance(a));		
 	}
 		
 	return true;
